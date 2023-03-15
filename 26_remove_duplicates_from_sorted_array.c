8a7950c076d9880328410c70b6e57e2ea88c4487
Hayden Howell <hayde@Haydens-MacBook-Air.local>
2023-03-15 03:29:37 -0400

    26 - remove duplicates

---
+++ 26_remove_duplicates_from_sorted_array.c
@@ -0,0 +1,22 @@
+/*
+ * -----------------------------------------------------------------------------
+ * see: https://leetcode.com/problems/remove-duplicates-from-sorted-array/ 
+ * time: O(nlogn)
+ * space:      O(n)
+ *
+ * Author -    Hayden Howell
+ * Email  -    hayde0264@gmail.com
+ * Date   -    03/11/2023
+ * -----------------------------------------------------------------------------
+ */
+
+int removeDuplicates(int* nums, int numsSize) {
+  int j = 1;
+  for (int i = 1; i < numsSize; i++) {
+    if (nums[i] != nums[i - 1]) {
+      nums[j] = nums[i];
+      j++;
+    }
+  }
+  return j;
+}
